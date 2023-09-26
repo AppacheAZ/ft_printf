923ea9f55aa9678e11a21eca31efa6f066088d06
AppacheAZ <marcoaz2001@outlook.com>
2023-09-26 20:14:42 +0200

    add README.md and format the test

---
+++ README.md
@@ -0,0 +1,43 @@
+# ft_printf
+This project recreate the popular function printf from the standard library in C.
+This project is part of the **42** school cursus and follows the Norme coding standard.
+
+this prototype have the following formats:
+- %c
+- %s
+- %p
+- %d
+- %i
+- %u
+- %x
+- %X
+- %%
+- ... more to come
+
+## Usage
+
+To compile the library, run `make` in the root of the repository. This will create a `libftprintf.a` file that you can link to your project.
+Use the following command to clean the object files:
+`make clean` to clean the object files
+`make fclean` to clean the object files and the library
+`make re` to recompile the library
+To see what does the Makefile do, use the following command: `make -n`
+
+## Example
+To test the library, you can use the following code:
+<pre><code>
+#include "ft_printf.h"
+int main(void)
+{
+    int myft;
+    int real;
+    char *str = "hola";
+    
+    myft = ft_printf("myft: Hol%c %s %p %d%i %u %x %X %%\n", 'a', "mundo", str, 4, 2, -42, 31, 232988557);
+    real = printf("real: Hol%c %s %p %d%i %u %x %X %%\n", 'a', "mundo", str, 4, 2, -42, 31, 232988557);
+
+    printf("\nmyft: %d\nreal: %d\n", myft, real);
+
+    return (0);
+}
+</code></pre>
--- ft_printf.c
+++ ft_printf.c
@@ -6,12 +6,11 @@
 /*   By: marcoalv <marcoalv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/19 11:15:58 by marcoalv          #+#    #+#             */
-/*   Updated: 2023/09/25 21:46:22 by marcoalv         ###   ########.fr       */
+/*   Updated: 2023/09/26 19:13:17 by marcoalv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-#include <stdio.h> /// BORRAR MEMBRILLO
 
 int	ft_printf(const char *format, ...)
 {
@@ -29,24 +28,3 @@ int	ft_printf(const char *format, ...)
 		return (-1);
 	return (printed_chars);
 }
-
-/*int main(void)
-{
-    int myft;
-    int real;
-    int myft2;
-    int real2;
-    char *str = "hola";
-    
-    myft = ft_printf("myft: Hol%c %s %p %d%i %u %x %X %%\n", 'a', "mundo", str, 4, 2, -42, 31, 232988557);
-    real = printf("real: Hol%c %s %p %d%i %u %x %X %%\n", 'a', "mundo", str, 4, 2, -42, 31, 232988557);
-
-    myft2 = ft_printf("%p", (void *)-14523);
-    write(1, "\n", 1);
-    real2 = printf("%p", (void *)-14523);
-
-    printf("\nmyft: %d\nreal: %d\n", myft, real);
-    printf("\nmyft: %d\nreal: %d\n", myft2, real2);
-
-    return (0);
-}*/
